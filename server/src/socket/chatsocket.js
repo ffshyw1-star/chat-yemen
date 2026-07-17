50ca4e072107b0784c9b0173546d865ab48a5ec1
ffshyw1-star <ffshyw1@gmail.com>
2026-07-17 05:16:47 +0300

    Update chatsocket.js

--- server/src/socket/chatsocket.js
+++ server/src/socket/chatsocket.js
@@ -1,38 +1,147 @@
+const db = require("../database/database");
+
+
 module.exports = (io)=>{
+
 
 io.on("connection",(socket)=>{
 
-console.log("User Connected",socket.id);
+
+console.log("User Connected:",socket.id);
 
 
-socket.on("join_room",(room)=>{
 
-socket.join(room);
+// دخول غرفة
 
-io.to(room).emit(
-"system_message",
-"انضم مستخدم جديد للغرفة"
+socket.on(
+"join_room",
+(data)=>{
+
+
+const roomId =
+data.room;
+
+
+const username =
+data.username;
+
+
+
+socket.join(
+"room_"+roomId
 );
+
+
+
+socket.username =
+username;
+
+socket.room =
+roomId;
+
+
+
+// رسالة النظام
+
+io.to("room_"+roomId)
+.emit(
+"system_message",
+
+`انضم ${username} إلى الغرفة`
+
+);
+
+
 
 });
 
 
-socket.on("send_message",(data)=>{
 
-io.to(data.room)
+
+// إرسال رسالة
+
+socket.on(
+"send_message",
+(data)=>{
+
+
+const {
+room,
+username,
+message
+}=data;
+
+
+
+const time =
+new Date()
+.toLocaleString(
+"ar-EG",
+{
+hour:"2-digit",
+minute:"2-digit",
+day:"2-digit",
+month:"2-digit"
+}
+);
+
+
+
+// إرسال للجميع
+
+io.to(
+"room_"+room
+)
 .emit(
 "receive_message",
-data
+{
+
+username,
+
+message,
+
+time
+
+}
+
 );
 
+
+
 });
 
 
-socket.on("disconnect",()=>{
 
-console.log("User Disconnected");
+
+
+// خروج المستخدم
+
+socket.on(
+"disconnect",
+()=>{
+
+
+if(socket.room && socket.username){
+
+
+io.to(
+"room_"+socket.room
+)
+.emit(
+
+"system_message",
+
+`غادر ${socket.username}`
+
+);
+
+
+}
+
+
 
 });
+
 
 
 });

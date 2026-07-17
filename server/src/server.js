cab7923cb3f1eb1f4f94970c92aa64abc66e114b
ffshyw1-star <ffshyw1@gmail.com>
2026-07-17 06:14:12 +0300

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,43 +1,69 @@
 const http = require("http");
+
 const app = require("./app");
+
+
+// تحميل جداول قاعدة البيانات
+
+require("./database/tables");
+
+
+// تحميل نظام الرتب والصلاحيات
+
+require("./database/ranks");
+
+
+
 const { Server } = require("socket.io");
 
 
-// إنشاء السيرفر
 
 const server = http.createServer(app);
 
 
-// إعداد Socket.IO
 
-const io = new Server(server, {
+const io = new Server(server,{
 
-    cors: {
-        origin: "*"
-    }
+cors:{
+
+origin:"*"
+
+}
 
 });
 
 
-// تشغيل أنظمة Socket
+
+
+// Socket العامة
 
 require("./socket/chatSocket")(io);
 
+
+// Socket الخاص
+
 require("./socket/privateSocket")(io);
+
+
+// Socket المتواجدين
 
 require("./socket/onlineSocket")(io);
 
 
 
-// المنفذ
-
-const PORT = process.env.PORT || 3000;
 
 
-// تشغيل الموقع
+const PORT =
+process.env.PORT || 3000;
 
-server.listen(PORT, ()=>{
 
-    console.log(`Chat Yemen Server Running ${PORT}`);
+
+server.listen(PORT,()=>{
+
+
+console.log(
+`Chat Yemen Server Running ${PORT}`
+);
+
 
 });
